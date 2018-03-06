7dbc40a4481bbd181bcf2e24a6fd83b06c994312
Stractus <charles-marchand@hotmail.fr>
2018-03-06 11:13:21 +0100

    oups :(

--- src/main/java/io/github/jhipster/application/web/rest/ApiController.java
+++ src/main/java/io/github/jhipster/application/web/rest/ApiController.java
@@ -1,5 +1,10 @@
 package io.github.jhipster.application.web.rest;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import javax.annotation.security.RolesAllowed;
 
 import org.springframework.http.HttpHeaders;
@@ -12,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.github.jhipster.application.security.AuthoritiesConstants;
-import io.github.jhipster.application.security.jwt.JWTConfigurer;
 
 @RestController
 @RequestMapping("/api")
@@ -27,14 +31,53 @@ public class ApiController {
 		/*
 		 * Acces à la blockChain 
 		 */
-		String digest = "digestcoucou "+Transaction;
+		
+		try {
+		//String url = "http://89.86.39.88:1880/UGA/get?transaction="+id;";
+		
+		String url = "http://192.168.1.9:1880/UGA/get?transaction="+Transaction;
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+				
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(con.getInputStream()));
+		
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+		String digest = response.toString();
 		d.setDigest(digest);
 		HttpHeaders httpHeaders = new HttpHeaders();
 		return new ResponseEntity<>(d, httpHeaders, HttpStatus.OK);
+		
+		
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			HttpHeaders httpHeaders = new HttpHeaders();
+			return new ResponseEntity<>(d, httpHeaders, HttpStatus.NOT_FOUND);
+		}
+		
+		
 	}
-	
-	
-	
 	
 	static class Digest{
 		private String digest; 
